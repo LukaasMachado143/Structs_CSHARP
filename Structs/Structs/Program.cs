a4621da35ede1a522c72253c0c256d34ff8943ce
Lucas Machado <lukaasmachado143@gmail.com>
2022-08-09 21:11:17 -0300

    Structs Revisada.

--- Structs/Structs/Program.cs
+++ Structs/Structs/Program.cs
@@ -8,8 +8,33 @@ namespace Structs
 {
     internal class Program
     {
+        
+        struct Car
+        {
+            string name;
+            string brand;
+            string fuelType;
+            float fuelTankCapacity;
+
+            public Car(string name,string brand, string fuelType, float fuelTankCapacity)
+            {
+                this.name = name;
+                this.brand = brand;
+                this.fuelType = fuelType;
+                this.fuelTankCapacity = fuelTankCapacity;
+            }
+
+            public string getName()
+            {
+                return this.name;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Car firstCar = new Car("Gol Qaudrado","Volkswagen","Gasolina",50.10f);
+            Console.WriteLine($"O nome do Automóvel é: {firstCar.getName()}");
+            Console.ReadLine();
         }
     }
 }
